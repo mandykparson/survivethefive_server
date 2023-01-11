aee5460ad02f08decc81fbd6a3a3c64cab369e0b
Mandy Parson <mandyparson@Mandys-MacBook-Air.local>
2023-01-11 14:57:37 -0700

    creating static grades

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+grades = Grade.create([
+    { name: 'Kindergarten'},
+    { name: '1st'},
+    { name: '2nd'},
+    { name: '3rd'},
+    { name: '4th'},
+    { name: '5th'},
+    { name: '6th'},
+    { name: '7th'},
+    { name: '8th'},
+    { name: '9th'},
+    { name: '10th'},
+    { name: '11th'},
+    { name: '12th'}
+])
